05749886328f8c37cafaacad87a99f7263e3f6a8
swati <131149003+2003swat@users.noreply.github.com>
2023-04-22 15:44:50 +0530

    Add files via upload

---
+++ patone.py
@@ -0,0 +1,6 @@
+rows = int(input("enter the number of rows: "))
+for a in range(rows):
+     for b in range(a+1):
+         print("* ", end="")
+     print("\n")    
+
---
+++ pattwo.py
@@ -0,0 +1,6 @@
+rows = int(input("enter the number of rows: "))
+
+for a in range(rows,0,-1):
+    for b in range(0,a):
+        print("* ", end="")
+    print("\n")    
